4aa73eb4ebccd7639efa0946b2e777e770c78ee3
Sam <yehtutaung@abank.com.mm>
2024-01-11 14:26:00 +0630

    simple landing page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Home, About, Work, Testimonial, Contact, Footer } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,18 @@
+import About from "./About"
+import Contact from "./Contact"
+import Footer from "./Footer"
+import Home from "./Home"
+import Navbar from "./Navbar"
+import Testimonial from "./Testimonial"
+import Work from "./Work"
+
+
+export {
+  About, 
+  Contact,
+  Footer,
+  Home, 
+  Navbar,
+  Testimonial,
+  Work
+}
